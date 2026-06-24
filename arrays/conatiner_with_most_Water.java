82485128ca47c6221f7796f1324e2085b5d50d83
Durga1534 <durgaprasadpc143@gmail.com>
2026-06-24 19:12:38 +0530

    solved container with most water problem using two pointers pattern

---
+++ arrays/conatiner_with_most_Water.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class conatiner_with_most_Water {
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int maxWater = 0;
+
+        while(left < right) {
+            maxWater = Math.max(maxWater, (right - left) * Math.min(height[left], height[right]));
+            if(height[left] < height[right]) {
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+    public static void main(String args[]) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+}
