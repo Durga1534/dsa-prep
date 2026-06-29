7bd8d132fe60210ae6aebe3c9ab3aad8472da636
Durga1534 <durgaprasadpc143@gmail.com>
2026-06-29 17:38:54 +0530

    solved move zeroes problem using fast & slow pointers approach without copying array

---
+++ arrays/move_zeroes.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class move_zeroes {
+
+    // Moves all zeros to the end of the array while maintaining relative order of non-zero elements.
+    public static void moveZeroes(int[] nums) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                int temp = nums[fast];
+                nums[fast] = nums[slow];
+                nums[slow] = temp;
+                slow++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+
